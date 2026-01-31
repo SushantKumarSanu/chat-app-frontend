c826c4d895f47d62ef281fc975ad635d51db290b
SushantKumarSanu <sushantkumar14507@gmail.com>
2026-01-31 20:10:46 +0530

    Redirect Authenticated user away from Login page

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,12 @@ import { Routes, Route } from "react-router-dom"
 import Login from "./pages/Login.jsx"
 import Chat from "./pages/chat.jsx"
 import AuthGuard from "./components/AuthGuard.jsx"
+import GuestGuard from "./components/GuestGuard.jsx"
 
 function App() {
   return (
     <Routes>
-      <Route path="/" element={<Login />} />
+        <Route path="/" element={<GuestGuard><Login /></GuestGuard>}/>
       <Route path="/chat" element={<AuthGuard><Chat/></AuthGuard>}/>
     </Routes>
   )
