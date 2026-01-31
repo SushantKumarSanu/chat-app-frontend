7fcbc30ca24c8ad7815dd3c02b290af3b5475f9f
SushantKumarSanu <sushantkumar14507@gmail.com>
2026-01-31 21:15:36 +0530

    loaded logged-in user on Chat page

--- src/pages/Chat.jsx
+++ src/pages/Chat.jsx
@@ -1,7 +1,26 @@
+import { useEffect, useState } from "react";
+import api from "../services/api.js";
+
+
 function Chat(){
+    const [username,setUsername] = useState(null)
+    useEffect(() => { 
+        (async()=>{
+        try{    
+        const res = await api.get("/api/protected/profile");
+        console.log("login info",res.data.user);
+        const user = res.data.user
+        setUsername(user.username);
+        }catch(error){
+            console.error(error.message);
+        }
+    })();
+},[]);
+
+
     return <>
     <div className="chats">
-        <h1>Chat Page</h1>
+        <h1>{username}</h1>
     </div>
     </>
 }
