de224b6b113cb2745aa4f63deaacc07365e9549b
Sushant Kumar Sanu <sushantkumar14507@gmail.com>
2026-05-04 02:12:40 +0530

    feat(search): implement debounced user search with API integration

--- src/components/organisms/ChatSidebar.jsx
+++ src/components/organisms/ChatSidebar.jsx
@@ -1,11 +1,75 @@
+import { useEffect, useRef, useState } from 'react';
+import 'remixicon/fonts/remixicon.css';
+import searchIcon from '../../assets/images/search-line.svg'
+import api from '../../services/api.js';
+
 function ChatSidebar({chatlist,user,loading,onSelectChat,otherUserActivity,unreadByChat}){
 
-    
+    const[query,setQuery]= useState("");
+    const[searchResult,setSearchResult] = useState([]);
+    const searchTimer = useRef(null);
+
+        useEffect(()=>{
+            if(searchTimer.current){
+                clearTimeout(searchTimer.current);
+            } 
+    },[]);
     return<>
     <div className="sidebar">
         <div className="sidebar-header">
             <div className="avatar">PFL</div>
             <span className="name">{user?.username}</span>
+        </div>
+        <div className="search-box">
+            <div className='search-section'>
+            <div className="search-icon">
+               <img src= {searchIcon} alt="" /> 
+            </div>
+            <div className="search-value"><input type="text" value={query} onChange={(e)=>{
+                const value = e.target.value;
+
+                setQuery(value);
+
+        
+                if(searchTimer.current) {
+                    clearTimeout(searchTimer.current)
+                };
+                
+                if(!value.trim()){
+                    setSearchResult([]);
+
+                    return;
+                }
+
+
+        
+                searchTimer.current =
+                    setTimeout(
+                        async()=>{
+                            try{
+                                    const res = await api.get(`/api/users/search?query=${value}`)
+                                    setSearchResult(res.data.result);
+
+                            }catch(error){
+                                    console.log(error.message);
+                            }
+                    },500);
+
+
+            }} placeholder='Search'/></div>
+            </div>
+            <div className="search-result">
+                {query && searchResult.length === 0 && (
+                <div className="search-card">No users found</div>
+                )}
+                {searchResult?.map((result)=>{
+                    return <div className='search-card' key={result._id}>
+                        <span>{result?.email}</span>
+                    </div>
+                })}
+                
+                
+                </div>
         </div>
             {
             chatlist?.map((elem)=>{
