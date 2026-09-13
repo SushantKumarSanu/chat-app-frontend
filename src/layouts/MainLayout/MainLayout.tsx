25d50c0daa8af5555c5e0d3fe7899ddab7c55d7e
Sushant Kumar Sanu <sushantkumar14507@gmail.com>
2026-09-13 12:59:31 +0530

    refactor: migrate MainLayout to TypeScript and remove user props

--- src/layouts/MainLayout/MainLayout.tsx
+++ src/layouts/MainLayout/MainLayout.tsx
@@ -1,22 +1,29 @@
-import { useState } from "react";
-import Chat from "../../features/chat/pages/Chat.jsx";
+import { JSX, useState } from "react";
+import Chat from "../../features/Chat/pages/Chat.jsx";
 import AppSidebar from "./components/AppSidebar.jsx";
 import FeatureShowCase from "./components/FeatureShowCase.jsx"
 import Profile from "../../features/Profile/pages/Profile.jsx"
 import Setting from "../../features/Setting/pages/Setting.jsx";
 
-function MainLayer({ user , setUser }){
-    const [activeView,setActiveView] = useState("chat");
+function MainLayer(){
+    type view = "chat"|"profile"|"setting"
+    const [activeView,setActiveView] = useState<view>("chat");
     const isChat = activeView === "chat";
 
-    const viewMap = {
-        "chat":()=> <Chat user={user} setUser={setUser}/>,
-        "profile":()=><Profile setUser={setUser} user={user}/>,
-        "setting": ()=><Setting setUser={setUser} user={user}/>
+    interface IViewMap{
+        "chat": () => JSX.Element,
+        "profile":()=>JSX.Element,
+        "setting":()=>JSX.Element
+    }
+
+    const viewMap:IViewMap = {
+        "chat":()=> <Chat />,
+        "profile":()=><Profile/>,
+        "setting": ()=><Setting />
     }
 
     return<><div className="bg-background text-on-background font-body-md h-screen flex overflow-hidden pb-16 md:pb-0 md:pl-20">
-        <AppSidebar user={user} selectView={setActiveView}/>
+        <AppSidebar selectView={setActiveView}/>
         {viewMap[activeView]?.()}
         {!isChat&& <FeatureShowCase/>}
         </div>
