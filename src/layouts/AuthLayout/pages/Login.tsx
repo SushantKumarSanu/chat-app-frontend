968c1544627cb8d6b4d843ff8883427d30824fdb
Sushant Kumar Sanu <sushantkumar14507@gmail.com>
2026-09-13 13:53:03 +0530

    refactor: migrate Login page  to TypeScript and remove user prop

--- src/layouts/AuthLayout/pages/Login.tsx
+++ src/layouts/AuthLayout/pages/Login.tsx
@@ -3,13 +3,13 @@ import IntroAnimation from "../components/IntroAnimation";
 import LoginForm from "../components/Login/LoginForm";
 import "../components/auth.css"
 
-function Login({setUser}){
+function Login(){
     return(
         <div className="signup-page">
             <IntroAnimation/>
             <div className="glass-card w-full max-w-md rounded-xl p-8 shadow-2xl flex flex-col gap-6 relative z-10">
 
-                <LoginForm setUser={setUser}/>
+                <LoginForm />
             </div>
             
 
