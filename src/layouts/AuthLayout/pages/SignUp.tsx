563b18a2b4e1d683775306d617aa7a049490aafa
Sushant Kumar Sanu <sushantkumar14507@gmail.com>
2026-09-13 13:51:21 +0530

    refactor: migrate signup to TypeScript and remove user prop

--- src/layouts/AuthLayout/pages/SignUp.tsx
+++ src/layouts/AuthLayout/pages/SignUp.tsx
@@ -1,16 +1,15 @@
 import IntroAnimation from "../components/IntroAnimation.jsx";
-// import "../components/SignUp/signUp.css"
 import "../components/auth.css"
 import SignUpForm from "../components/SignUp/SignUpForm.jsx";
 import SocialLogin from "../components/SignUp/SocialLogin.jsx";
 import SignUpFooter from "../components/SignUp/SighUpFooter.jsx";
 
-function SignUp({setUser}){
+function SignUp(){
 
     return<><div className="signup-page">
         <IntroAnimation/>
         <div className="glass-card w-full max-w-md rounded-xl p-8 shadow-2xl flex flex-col gap-6 relative z-10">
-        <SignUpForm setUser={setUser}/>
+        <SignUpForm />
         <SocialLogin/>
         <SignUpFooter/>
         </div>
