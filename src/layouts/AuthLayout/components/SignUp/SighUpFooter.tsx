d51a8b78baf6e73db0b0f3f9d93f72bcf17081f7
Sushant Kumar Sanu <sushantkumar14507@gmail.com>
2026-09-13 14:45:36 +0530

    refactor: migrate SignUp Footer  to TypeScript

---
+++ src/layouts/AuthLayout/components/SignUp/SighUpFooter.tsx
@@ -0,0 +1,11 @@
+
+
+function SignUpFooter(){
+    return <><p className="text-center font-body-md text-body-md text-on-surface-variant mt-2">
+                Have an account already?  
+                <a className="text-primary hover:text-on-surface transition-colors font-medium" href="/">  LogIn here</a>
+            </p>
+        </>
+};
+
+export default SignUpFooter;
