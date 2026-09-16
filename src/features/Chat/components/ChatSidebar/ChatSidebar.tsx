5ca531b033bc162622066fc0e7b15531b8a23725
Sushant Kumar Sanu <sushantkumar14507@gmail.com>
2026-09-16 22:41:04 +0530

    refactor: remove Chat state props from ChatSidebar and migrate to TypeScript

--- src/features/Chat/components/ChatSidebar/ChatSidebar.tsx
+++ src/features/Chat/components/ChatSidebar/ChatSidebar.tsx
@@ -3,16 +3,16 @@ import ChatList from './ChatList.jsx';
 import SidebarHeader from './SidebarHeader.jsx';
 
 
-function ChatSidebar({chatlist,user,loading,onSelectChat,activeChat,otherUserActivity,setChatlist}){
+function ChatSidebar(){
 
 
     return<>
 
 <div className="w-full md:w-sidebar-width h-full bg-surface-container-low/80 backdrop-blur-xl border-r border-outline-variant/30 hidden md:flex flex-col shrink-0 relative z-40">
    
-        <SidebarHeader user={user}/>
+        <SidebarHeader/>
         <SearchBox/>
-        <ChatList  chatlist={chatlist} user={user} activeChat={activeChat} onSelectChat={onSelectChat} otherUserActivity={otherUserActivity} setChatlist={setChatlist}/>
+        <ChatList/>
             
         </div> 
     </>
